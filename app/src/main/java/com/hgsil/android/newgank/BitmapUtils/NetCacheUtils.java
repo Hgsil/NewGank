e9fa8f0d6ed97dad92ad813969a34b44e91443b9
Hgsil <az401366794@qq.com>
2017-03-11 21:54:20 +0800

    ......

--- app/src/main/java/com/hgsil/android/newgank/BitmapUtils/NetCacheUtils.java
+++ app/src/main/java/com/hgsil/android/newgank/BitmapUtils/NetCacheUtils.java
@@ -1,12 +1,9 @@
 package com.hgsil.android.newgank.BitmapUtils;
 
-import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.widget.ImageView;
-
-
 
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -29,9 +26,41 @@ public class NetCacheUtils {
      * @param ivPic 显示图片的imageview
      * @param url   下载图片的网络地址
      */
-    public void getBitmapFromNet(ImageView ivPic, String url,Context context,boolean isDownLoad) {
-        new BitmapTask().execute(ivPic, url,context,isDownLoad);//启动AsyncTask
+    public void getBitmapFromNet(ImageView ivPic, String url, String id) {
+        new BitmapTask().execute(ivPic, url, id);//启动AsyncTask
 
+    }
+
+    /**
+     * 网络下载图片
+     * @param url
+     * @return
+     */
+    private Bitmap downLoadBitmap(String url) {
+
+        HttpURLConnection conn = null;
+        try {
+            conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setConnectTimeout(5000);
+            conn.setReadTimeout(5000);
+            conn.setRequestMethod("GET");
+
+            int responseCode = conn.getResponseCode();
+            if (responseCode == 200) {
+                //图片压缩
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;//宽高压缩为原来的1/2
+                options.inPreferredConfig = Bitmap.Config.ARGB_4444;
+                Bitmap bitmap = BitmapFactory.decodeStream(conn.getInputStream(), null, options);
+                return bitmap;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            conn.disconnect();
+        }
+
+        return null;
     }
 
     /**
@@ -44,8 +73,8 @@ public class NetCacheUtils {
 
         private ImageView ivPic;
         private String url;
-        private Context mContext;
-        private boolean isDownLoad;
+        private String id;
+
 
         /**
          * 后台耗时操作,存在于子线程中
@@ -56,9 +85,7 @@ public class NetCacheUtils {
         protected Bitmap doInBackground(Object[] params) {
             ivPic = (ImageView) params[0];
             url = (String) params[1];
-            mContext = (Context) params[2];
-            isDownLoad = (boolean) params[3];
-
+            id = (String) params[2];
             return downLoadBitmap(url);
         }
 
@@ -82,45 +109,12 @@ public class NetCacheUtils {
                 System.out.println("从网络缓存图片啦.....");
 
                 //从网络获取图片后,保存至本地缓存
-                mLocalCacheUtils.setBitmapToLocal(url, result,isDownLoad,mContext);
-                if (!isDownLoad)
+                mLocalCacheUtils.setBitmapToLocal(id, result);
                 //保存至内存中
                 mMemoryCacheUtils.setBitmapToMemory(url, result);
 
             }
         }
-    }
-
-    /**
-     * 网络下载图片
-     * @param url
-     * @return
-     */
-    private Bitmap downLoadBitmap(String url) {
-
-        HttpURLConnection conn = null;
-        try {
-            conn = (HttpURLConnection) new URL(url).openConnection();
-            conn.setConnectTimeout(5000);
-            conn.setReadTimeout(5000);
-            conn.setRequestMethod("GET");
-
-            int responseCode = conn.getResponseCode();
-            if (responseCode == 200) {
-                //图片压缩
-                BitmapFactory.Options options = new BitmapFactory.Options();
-                options.inSampleSize=2;//宽高压缩为原来的1/2
-                options.inPreferredConfig=Bitmap.Config.ARGB_4444;
-                Bitmap bitmap = BitmapFactory.decodeStream(conn.getInputStream(),null,options);
-                return bitmap;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            conn.disconnect();
-        }
-
-        return null;
     }
 
 }
